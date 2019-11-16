d6d688f05afe8b756d95b8d54ad5c4b4788692c4
Bogdan <bogdankorzh254@mail.ru>
2019-11-16 13:20:45 +0300

    AVL tree bug fixed

--- 07/avl-tree.cpp
+++ 07/avl-tree.cpp
@@ -1,5 +1,6 @@
 #ifndef AVL_TREE
 #define AVL_TREE
+#include <iostream>
 
 namespace saod
 {
@@ -14,7 +15,7 @@ public:
     TreeNode * left, 
              * right;
 
-    TreeNode * insert (TreeNode * node, int key);
+    void print();
 
 };
 
@@ -24,6 +25,16 @@ TreeNode::TreeNode(int d)
     left = right = nullptr;
     height = 1;
     data = d;
+}
+
+
+void TreeNode::print() 
+{
+    std::cout << data << " ";
+    if (left != nullptr)
+        left->print();
+    if (right != nullptr)
+        right->print();
 }
 
 
@@ -40,20 +51,66 @@ private:
 
     TreeNode * rightRotate(TreeNode * y);
     TreeNode * leftRotate(TreeNode * x);
+    // внутренний алгоритм вставки
+    TreeNode * _insert(TreeNode * node, int data); 
 
 public: 
-    TreeNode * insert(TreeNode * node);
+    void insert(int data) { 
+        // std::cout << data;
+        root = _insert(root, data); 
+    }
+    void print() { root->print(); }
 
 };
 
 
-TreeNode * Tree::insert(TreeNode * node)
-{
 
+TreeNode * Tree::_insert(TreeNode * node, int data)
+{
+    /* 1. Обычная вставка элемента в бинарное дерево поиска */
+
+    if (node == nullptr)
+        return new TreeNode(data);
+
+    if (data < node->data)
+        node->left = _insert(node->left, data);
+    else if (data > node->data)
+        node->right = _insert(node->right, data);
+    else  // одиинаковые узлы не допускаются
+        return node; // ???
+
+    /* 2. Обновить высоту этого предка  */
+
+    node->height = 1 + _max(height(node->left), height(node->right));
+
+    /* 3. Получить значение баланса чтобы проверить, не нарушился ли баланс */
+    int balance = getBalance(node);
+
+    // Left Left Case
+    if (balance > 1 && data < node->left->data)
+        return rightRotate(node);
+    
+    // Right Right Case
+    if (balance < -1 && data > node->right->data)
+        return leftRotate(node);
+
+    // Left Right Case
+    if (balance > 1 && data > node->left->data) {
+        node->left = leftRotate(node->left);
+        return rightRotate(node);
+    }
+
+    // Right Left Case
+    if (balance < -1 && data < node->right->data) {
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+
+    return node;
 }
 
 
-int height(TreeNode * n)
+int Tree::height(TreeNode * n)
 {
     if (n == nullptr)
         return 0;
@@ -61,7 +118,7 @@ int height(TreeNode * n)
 }
 
 
-int _max(int a, int b)
+int Tree::_max(int a, int b)
 {
     return (a > b) ? a : b;
 }
@@ -75,8 +132,8 @@ TreeNode * Tree::rightRotate(TreeNode * y)
     x->right = y;
     y->left = T2;
 
-    y->height = _max(height(y->left), height(y->right) + 1);
-    x->height = _max(height(x->left), height(x->right) + 1);
+    y->height = _max(height(y->left), height(y->right)) + 1;
+    x->height = _max(height(x->left), height(x->right)) + 1;
 
     return x;
 }
@@ -90,8 +147,8 @@ TreeNode * Tree::leftRotate(TreeNode * x)
     y->left = x;
     x->right = T2;
 
-    x->height = _max(height(x->left), height(x->right) + 1);
-    y->height = _max(height(y->left), height(y->right) + 1);
+    x->height = _max(height(x->left), height(x->right)) + 1;
+    y->height = _max(height(y->left), height(y->right)) + 1;
 
     return y;
 }
--- 07/main.cpp
+++ 07/main.cpp
@@ -4,5 +4,15 @@
 
 int main ()
 {
-    
+    saod::Tree avl;
+
+    avl.insert(10);
+    avl.insert(20);
+    avl.insert(30);
+    avl.insert(40);
+    avl.insert(50);
+    avl.insert(25);
+
+    avl.print();
+
 }
